ab659a095be2b60b8d672af2cbf89f2b7fdbd5d4
snowsphere <info@snowsphere.net>
2024-03-26 06:45:46 +0900

    fix: ビルドエラーに関する内容を修正

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue'
-import AddBtn from './components/AddBtn.vue'
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
 </script>
 
 <template>
   <NavBar></NavBar>
-  <!-- <AddBtn class="fixed bottom-0 right-0 m-4 z-20" @click="router.push('/add')"></AddBtn> -->
   <div class="container px-8 my-4 mx-auto">
     <RouterView></RouterView>
   </div>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -6,7 +6,7 @@ import Card from './Card.vue'
   <div>
     <div class="text-xl font-bold">ららぽーと新三郷</div>
     <div class="mt-2 flex overflow-scroll gap-4 w-full py-4">
-      <Card v-for="n in 8"></Card>
+      <Card v-for="n in 8" :key="n"></Card>
       <Card></Card>
     </div>
   </div>
