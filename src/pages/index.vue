2e9872535aba7e0dd04f5bf95ed5d9c420979d63
snowsphere <info@snowsphere.net>
2024-04-02 17:13:31 +0900

    feat: 注意事項のページをセッション中に複数回表示しないように変更

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,19 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const onConfirm = () => {
+  sessionStorage.setItem('confirm_notice', 'ok')
+  router.push('/list')
+}
+
+onMounted(() => {
+  if (sessionStorage.getItem('confirm_notice') === 'ok') {
+    router.push('/list')
+  }
+})
 </script>
 
 <template>
@@ -9,15 +23,23 @@ import { RouterLink } from 'vue-router'
     <li>某アミューズメント施設のプリペイドカードの保存、表示が可能なアプリ</li>
     <li>カードに記されたQRコードと同じものを表示</li>
     <li>有効期限をカレンダーにリマインドとして追加可能</li>
-    <li>オフラインで動作可能（ここで扱うデータはすべて端末上で管理します）</li>
+    <li>オフラインで動作可能（ここで登録したデータはすべて端末上で管理します）</li>
   </ul>
   <div class="text-lg font-bold mt-4">注意事項</div>
   <ul class="list-disc list-inside mt-2">
-    <li>本アプリは元サービスの運営会社側の許諾を得て開発したものではありません。カードに関するシステムの権利等は運営会社に帰属します。</li>
-    <li>完全な動作の保証はできかねます。本アプリ利用時も発行したカードは必ず保管、携帯し自己責任で利用してください。</li>
-    <li>本アプリに関する内容に関して、元サービスの運営会社へのお問い合わせはご遠慮ください。</li>
+    <li>
+      本アプリは元サービスの運営会社側の許諾を得て開発したものではありません。カードに関するシステムの権利等は運営会社に帰属します。
+    </li>
+    <li>
+      完全な動作の保証はできかねます。本アプリ利用時も発行したカードは必ず保管、携帯し自己責任で利用してください。
+    </li>
+    <li>
+      本アプリに関する内容に関して、元サービスの運営会社へのお問い合わせはご遠慮ください。
+    </li>
   </ul>
   <div class="text-center mt-6">
-    <RouterLink to="/list" class="btn btn-error">注意事項を理解した上で利用を開始する</RouterLink>
+    <button class="btn btn-error" @click="onConfirm">
+      注意事項を理解した上で利用を開始する
+    </button>
   </div>
 </template>
