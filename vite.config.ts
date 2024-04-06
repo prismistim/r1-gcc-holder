5e7b883b0bc07a7747556adc39409d65c2ab251f
snowsphere <info@snowsphere.net>
2024-04-06 23:02:44 +0900

    feat: PWA用のアセットの追加

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,31 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({ registerType: 'autoUpdate', injectRegister: 'auto' }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'r1-gcc-holder',
+        short_name: 'gcch',
+        description: 'Game Charge Card Holder for R1',
+        theme_color: '#000',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
   ],
 })
