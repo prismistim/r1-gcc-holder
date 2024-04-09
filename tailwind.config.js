451050153e673bf1db35c35c589df8b4a25503b9
snowsphere <info@snowsphere.net>
2024-04-10 03:03:59 +0900

    fix: require()でdaisyuiを読み込んでいたのをimportに変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [daisyui],
   daisyui: {
     themes: [
       {
