baec0b9e802dea8b25e4dd362d5c3bdf69305ff4
snowsphere <info@snowsphere.net>
2024-05-05 06:36:32 +0900

    change: 初期化を呼び出す場所をindexに変更し、初期化処理も修正

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,23 +1,7 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
-import { RouterLink, useRouter } from 'vue-router'
-import { useStore } from '../composables/useStore'
+import { onMounted } from 'vue'
+import { RouterLink } from 'vue-router'
 import { themeChange } from 'theme-change'
-
-const router = useRouter()
-const store = useStore()
-
-const isAgreeDelete = ref(false)
-
-const goToIndexPage = () => {
-  window.sessionStorage.clear()
-  router.push('/')
-}
-
-const initData = () => {
-  store.initStore()
-  location.reload()
-}
 
 onMounted(() => {
   themeChange(false)
@@ -35,8 +19,13 @@ onMounted(() => {
     </div>
     <div class="flex-none">
       <ul class="menu menu-horizontal rounded-box gap-x-2 text-rose-500">
-        <li v-show="$route.path !== '/'">
-          <button @click="goToIndexPage">
+        <li>
+          <button :disabled="$route.name === 'List'" @click="$router.push('/list')">
+            <span class="material-symbols-outlined">home</span>
+          </button>
+        </li>
+        <li>
+          <button @click="$router.push('/')">
             <span class="material-symbols-outlined">info</span>
           </button>
         </li>
@@ -47,47 +36,7 @@ onMounted(() => {
             <span class="material-symbols-outlined swap-on">dark_mode</span>
           </label>
         </li> -->
-        <li v-show="$route.path !== '/'">
-          <button :disabled="$route.name === 'Home'" onclick="delete_modal.showModal()">
-            <span class="material-symbols-outlined">delete</span>
-          </button>
-        </li>
       </ul>
     </div>
   </div>
-  <dialog id="delete_modal" class="modal">
-    <div class="modal-box">
-      <h3 class="font-bold text-lg">データの初期化</h3>
-      <p class="mt-2">
-        初期化を行うと、現在登録されているデータはすべて削除されます。よろしいですか？
-      </p>
-      <div class="form-control mt-6">
-        <label class="cursor-pointer flex gap-x-2 ">
-          <input
-            v-model="isAgreeDelete"
-            type="checkbox"
-            class="checkbox checkbox-error"
-          />
-          <span class="label-text">問題ない</span>
-        </label>
-      </div>
-      <div class="modal-action">
-        <div class="grid grid-cols-2 gap-x-4">
-          <form method="dialog">
-            <button class="btn">キャンセル</button>
-          </form>
-          <button
-            class="btn btn-error text-white"
-            :disabled="!isAgreeDelete"
-            @click="initData"
-          >
-            初期化
-          </button>
-        </div>
-      </div>
-    </div>
-    <form method="dialog" class="modal-backdrop">
-      <button>close</button>
-    </form>
-  </dialog>
 </template>
