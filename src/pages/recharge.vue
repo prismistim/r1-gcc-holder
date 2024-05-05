7172b900d9e4af03e8de0c157a59bbe1cc3d2535
snowsphere <info@snowsphere.net>
2024-05-05 11:07:14 +0900

    change: 再チャージの画面を現在のデザインに置き換え

--- src/pages/recharge.vue
+++ src/pages/recharge.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import dayjs from 'dayjs'
 import { useStore } from '../composables/useStore'
 import VueDatePicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
+import BackBtn from '@/components/BackBtn.vue'
 
 const router = useRouter()
 const route = useRoute()
 const store = useStore()
 
-const formData = ref<{ issueDate: Date }>({
-  issueDate: new Date(),
+const formData = ref<{ issueDate: string }>({
+  issueDate: '',
 })
 
 const setFormData = () => {
@@ -24,34 +25,54 @@ const setFormData = () => {
   router.push('/list')
 }
 
+const today = computed(() => {
+  return dayjs().format('YYYY-MM-DD')
+})
+
 if (!route.params.id || typeof route.params.id !== 'string') {
   router.push({ name: 'List' })
 }
+
+onMounted(() => {
+  formData.value.issueDate = today.value
+})
 </script>
 
 <template>
-  <div class="text-xl font-bold">有効期限を更新する（再チャージ）</div>
+  <BackBtn></BackBtn>
+  <div class="mt-2 text-xl font-bold">有効期限を更新する（再チャージ）</div>
   <div class="mt-4">
     <div class="mt-4">
       <label>再チャージ日</label>
       <div class="mt-2">
-        <button class="btn" @click="formData.issueDate = new Date()">
-          今日
-        </button>
         <VueDatePicker
           v-model="formData.issueDate"
           :enable-time-picker="false"
           format="yyyy-MM-dd"
-          dark
           auto-apply
           :min-date="store.getTargetItem(parseInt(route.params.id as string))?.issueDate"
           :max-date="new Date()"
+          input-class-name="dp-issue-date"
           class="mt-2"
         ></VueDatePicker>
+        <button class="btn mt-2" @click="formData.issueDate = today">
+          今日
+        </button>
       </div>
     </div>
     <div class="mt-6 text-center">
-      <button class="btn btn-primary" @click="setFormData">更新</button>
+      <button class="btn w-48 btn-primary text-white" @click="setFormData">
+        追加
+      </button>
     </div>
   </div>
 </template>
+
+<style>
+.dp-issue-date {
+  padding-top: 0;
+  padding-bottom: 0;
+  height: 3rem;
+  min-height: 3rem;
+}
+</style>
