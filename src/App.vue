a4513f96c982c8cc83852f0fb38373bb9eec5f25
snowsphere <info@snowsphere.net>
2024-05-15 07:51:18 +0900

    change: パッチ処理を関数化

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,43 @@ import NavBar from './components/NavBar.vue'
 import MyFooter from './components/MyFooter.vue'
 import UpdateNotificationModal from './components/UpdateNotificationModal.vue'
 import { useStore } from './composables/useStore'
+import { onMounted } from 'vue'
 
 const { needRefresh, updateServiceWorker } = useRegisterSW()
 const store = useStore()
+const patchStatus = localStorage.getItem('patch_status')
 
 if (needRefresh) {
   updateServiceWorker(true)
 }
 
-const patchStatus = localStorage.getItem('patch_status')
-
-// カードのオブジェクト構造のアップデート
-if (!patchStatus || parseInt(patchStatus) < 1) {
-  const storedData = store.getStoredData()
-
-  const updatedData = storedData?.cards.map((item) => {
-    return {
-      ...item,
-      isDummy: false
-    }
-  })
-
-  store.initStore()
-
-  updatedData?.forEach((item) => {
-    store.addItem(item)
-  })
+const updateStoredData = () => {
+  // カードのオブジェクト構造のアップデート
+  if (!patchStatus || parseInt(patchStatus) < 1) {
+    const storedData = store.getStoredData()
+  
+    if (!storedData) {
+      return
+    } 
+  
+    const updatedData = storedData?.cards.map((item) => {
+      return {
+        ...item,
+        isDummy: false
+      }
+    })
+  
+    store.initStore()
+  
+    updatedData?.forEach((item) => {
+      store.addItem(item)
+    })
+  }
 }
+
+onMounted(() => {
+  updateStoredData()
+})
 </script>
 
 <template>
