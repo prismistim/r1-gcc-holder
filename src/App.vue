525e88a0e912c2edb5d79ae8430f8d6754b0ee49
snowsphere <info@snowsphere.net>
2024-05-15 07:39:38 +0900

    feat: カードに関する型のアップデート処理を追加

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,33 @@ import { useRegisterSW } from 'virtual:pwa-register/vue'
 import NavBar from './components/NavBar.vue'
 import MyFooter from './components/MyFooter.vue'
 import UpdateNotificationModal from './components/UpdateNotificationModal.vue'
+import { useStore } from './composables/useStore'
 
 const { needRefresh, updateServiceWorker } = useRegisterSW()
+const store = useStore()
 
 if (needRefresh) {
   updateServiceWorker(true)
+}
+
+const patchStatus = localStorage.getItem('patch_status')
+
+// カードのオブジェクト構造のアップデート
+if (!patchStatus || parseInt(patchStatus) < 1) {
+  const storedData = store.getStoredData()
+
+  const updatedData = storedData?.cards.map((item) => {
+    return {
+      ...item,
+      isDummy: false
+    }
+  })
+
+  store.initStore()
+
+  updatedData?.forEach((item) => {
+    store.addItem(item)
+  })
 }
 </script>
 
